5665ab0c61eb492daa8dacc72436d1b30272ff5d
Louis Makower <louismakower@gmail.com>
2026-05-21 11:37:51 +0100

    add general RL runner to allow both agents to be used in one class

---
+++ louis_rl/algorithm.py
@@ -0,0 +1,21 @@
+from .ppo import PPORunner
+from .sac import SACRunner
+
+class RLRunner:
+    def __init__(self, env, cfg, log_dir, writer):
+        if cfg.algo_name.lower() == "ppo":
+            self.runner = PPORunner(env, cfg, log_dir, writer)
+        elif cfg.algo_name.lower() == "sac":
+            self.runner = SACRunner(env, cfg, log_dir, writer)
+
+    def learn(self):
+        self.runner.learn()
+
+    def load_checkpoint(self, path: str):
+        self.runner.load_checkpoint(path)
+
+    def get_deterministic_action(self, obs):
+        return self.runner.get_deterministic_action(obs)
+    
+    def add_goal_obs(self, obs):
+        return self.runner.add_goal_obs(obs)
